e9006481400a4c7ba9c176313ea417f2b3191ce4
amyhu0114 <amyhu0114@gmail.com>
2021-03-30 10:19:10 -0500

    Add Card

--- UI_React/src/App.js
+++ UI_React/src/App.js
@@ -1,21 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import "./styles.css"
+import Heading from "components/Heading/Heading.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
 import Charts from './views/Charts/Charts'
-{/*import React from "react";*/}
+import Card from './components/Card/Card'
+import CardHeader from './components/Card/CardHeader'
+import CardBody from './components/Card/CardBody'
+import CardText from "components/Card/CardText.js";
+const ppl = 10
 
 const App = () => {
-  {/*const message = "This is my first variable rendered in JSX!"
-  
-  const handleClick = () =>{
-    alert("you clicked the message!");
-    }
-  
-  const contacts = [
-      { name: "Jenny Han", email: "jenny.han@notreal.com", age: 25 },
-      { name: "Jason Long", email: "jason.long@notreal.com", age: 45 },
-      { name: "Peter Pan", email: "peter.pan@neverland.com", age: 100 },
-      { name: "John Harry", email: "john.harry@test.com", age: 28 }
-  ];*/}
   const [contacts, setContacts] = useState([]);
 
   useEffect(() => {
@@ -25,64 +20,24 @@ const App = () => {
         setContacts(data.results);
       });
   }, []);
-
+  
   return (
     <>
+    <Heading title="People Counter Report" textAlign="center" />
+      <GridContainer>
+        <GridItem xs={120}>
+        <Card>
+            <CardHeader color="warning" text>
+              <CardText color="warning">
+                <h3>Current Number of People Inside the Building: {ppl}</h3>
+              </CardText>
+            </CardHeader>
+          </Card>
+        </GridItem>
+      </GridContainer>
     <Charts/>
     </>
   );
 }
 
-const ContactCard = props => {
-  const [showAge, setShowAge] = useState(false);
-
-  return (
-    <>
-    <Charts/>
-    </>
-  );
-};
-
 export default App;
-
-{/*Goes in return() fir App()
-<div>
-  <h1>Hello React World</h1>
-  <h2 onClick={handleClick}>{message}</h2>
-  <h3 onClick={()=> alert("you clicked the message!")}>{message}</h3>
-</div>
-
-    <>
-      <ContactCard
-        avatar="https://via.placeholder.com/150"
-        name="Jenny Han"
-        email="jenny.han@notreal.com"
-        age={25}
-      />
-
-      <ContactCard
-        avatar="https://via.placeholder.com/150"
-        name="Jason Long"
-        email="jason.long@notreal.com"
-        age={45}
-      />
-
-      <ContactCard
-        avatar="https://via.placeholder.com/150"
-        name="Peter Pan"
-        email="peter.pan@neverland.com"
-        age={100}
-      />
-    </>
-
-    <>
-    {contacts.map(contact => (
-      <ContactCard
-        avatar="https://via.placeholder.com/150"
-        name={contact.name}
-        email={contact.email}
-        age={contact.age}
-      />
-    ))}
-  </>
-*/}
