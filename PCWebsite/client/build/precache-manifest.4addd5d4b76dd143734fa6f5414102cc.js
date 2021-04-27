6cb0bff88dd1fe5973f640b9519fa46e852ee0ef
amyhu0114 <amyhu0114@gmail.com>
2021-04-27 10:21:30 -0500

    Pls work

---
+++ PCWebsite/client/build/precache-manifest.4addd5d4b76dd143734fa6f5414102cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5ef03484b8d4af66798ab5f386b406c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c19ac6d78bdb05947e6c",
+    "url": "/static/css/main.5ac5a62c.chunk.css"
+  },
+  {
+    "revision": "2312e9c6310fc11c75d3",
+    "url": "/static/js/2.cb75b91d.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.cb75b91d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c19ac6d78bdb05947e6c",
+    "url": "/static/js/main.10a3d097.chunk.js"
+  },
+  {
+    "revision": "fd3afc898ee2b402ecf9",
+    "url": "/static/js/runtime-main.f9044392.js"
+  }
+]);
